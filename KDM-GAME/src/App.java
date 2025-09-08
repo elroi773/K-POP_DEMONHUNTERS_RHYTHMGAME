c6f005e75444e861a578196fc0023ff44a8fc13b
hamhanhae <s2430@e-mirim.hs.kr>
2025-09-08 14:13:42 +0900

    commit test

--- KDM-GAME/src/App.java
+++ KDM-GAME/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("깃허브 협업 테스트중 /이레");
+        System.out.println("깃허브 협업 테스트중 /한해");
     }
 }
