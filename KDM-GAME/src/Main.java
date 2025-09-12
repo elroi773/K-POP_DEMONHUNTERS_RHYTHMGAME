58774abdcc683dc3669a83491abc840db5f8d0d6
elroi773 <s2457@e-mirim.hs.kr>
2025-09-12 09:09:51 +0900

    창 띄우고 노래 선택하는 화면 하는중

--- KDM-GAME/src/Main.java
+++ KDM-GAME/src/Main.java
@@ -1,23 +1,53 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) {
         // 레이아웃
         BorderPane root = new BorderPane();
+
+        // 배경 이미지
+        BackgroundImage bgImage = new BackgroundImage(
+                new Image("file:../resources/img/background.png"),
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.CENTER,
+                new BackgroundSize(800, 600, false, false, false, false)
+        );
+        root.setBackground(new Background(bgImage));
 
         // 타이틀 텍스트
         Text title = new Text("K-POP DEMON HUNTERS - Rhythm Game");
         title.setFill(Color.DEEPPINK);
         title.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
 
-        root.setCenter(title);
+        // 제목 가운데 정렬
+        HBox titleBox = new HBox(title);
+        titleBox.setAlignment(Pos.CENTER);
+        root.setTop(titleBox);
+
+        // 버튼
+        Button selectSongBtn = new Button("게임 시작");
+        selectSongBtn.setStyle("-fx-font-size: 18px; -fx-background-color: pink;");
+
+        // 버튼 아래쪽 가운데 정렬
+        HBox buttonBox = new HBox(selectSongBtn);
+        buttonBox.setAlignment(Pos.CENTER);
+        buttonBox.setStyle("-fx-padding: 20px;"); // 버튼과 화면 끝 여백
+        root.setBottom(buttonBox);
+
+        // 버튼 이벤트 → SelectSong 화면으로 전환
+        selectSongBtn.setOnAction(e -> {
+            new SelectSong().show(primaryStage);
+        });
 
         // 씬
         Scene scene = new Scene(root, 800, 600);
@@ -27,11 +57,11 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
 
-        // TODO: MusicPlayer 실행 → OST 재생
-        // TODO: GameController 호출 → 게임 시작
+        // OST 재생
+        MusicPlayer.play("../resources/music/HowIt'sdone.mp3", true);
     }
 
     public static void main(String[] args) {
-        launch(args); // JavaFX 앱 실행
+        launch(args);
     }
 }
